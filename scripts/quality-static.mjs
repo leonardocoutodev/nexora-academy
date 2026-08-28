e16bb52e05d37d2ee761d51693f5133a06341d5f
leonardocoutodev <leonardo.couto.santos96@gmail.com>
2026-08-28 15:38:23 -0300

    Scope static quality checks to rendered HTML

--- scripts/quality-static.mjs
+++ scripts/quality-static.mjs
@@ -8,12 +8,13 @@ walk(publicDir);
 
 for(const file of htmlFiles){
   const src=fs.readFileSync(file,'utf8');
-  if(!/<html[^>]+lang=["']pt-BR["']/i.test(src))failures.push(file+': lang pt-BR ausente');
-  if(!/<meta[^>]+name=["']viewport["']/i.test(src))failures.push(file+': viewport ausente');
-  const ids=[...src.matchAll(/\bid=["']([^"']+)["']/g)].map(m=>m[1]);
+  const staticHtml=src.replace(/<script\b[^>]*>[\s\S]*?<\/script>/gi,'').replace(/<style\b[^>]*>[\s\S]*?<\/style>/gi,'');
+  if(!/<html[^>]+lang=["']pt-BR["']/i.test(staticHtml))failures.push(file+': lang pt-BR ausente');
+  if(!/<meta[^>]+name=["']viewport["']/i.test(staticHtml))failures.push(file+': viewport ausente');
+  const ids=[...staticHtml.matchAll(/\bid=["']([^"']+)["']/g)].map(m=>m[1]);
   const dup=ids.filter((x,i)=>ids.indexOf(x)!==i);
   if(dup.length)failures.push(file+': IDs duplicados '+[...new Set(dup)].join(', '));
-  if(/<img(?![^>]*\balt=)[^>]*>/i.test(src))failures.push(file+': imagem sem alt');
+  if(/<img(?![^>]*\balt=)[^>]*>/i.test(staticHtml))failures.push(file+': imagem sem alt');
 }
 const css=fs.readFileSync('public/assets/css/academy-v3.css','utf8');
 if(!css.includes('@media(max-width:820px)'))failures.push('academy-v3.css: breakpoint mobile principal ausente');
