f28f559d0ea7925d8238a9290103c69ea54d19ae
leonardocoutodev <leonardo.couto.santos96@gmail.com>
2026-08-29 09:02:45 -0300

    Include support page in Lighthouse budgets

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -4,7 +4,8 @@ module.exports={
       url:[
         'http://127.0.0.1:8787/',
         'http://127.0.0.1:8787/pages/login.html',
-        'http://127.0.0.1:8787/pages/cadastro.html'
+        'http://127.0.0.1:8787/pages/cadastro.html',
+        'http://127.0.0.1:8787/pages/apoie.html'
       ],
       numberOfRuns:1,
       settings:{
